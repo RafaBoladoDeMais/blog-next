380cb37d9690def845662496973d4e1ffb8ef8b2
rafael_verao@ufms.br <rafaelverao@MacBook-Pro-de-rafael.local>
2025-08-09 13:17:31 -0400

    criando postmodel e posts.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import clsx from "clsx";
+
 export default function HomePage(){
-  return <h1>Hello from page.tsx</h1>
-}
+  return (
+    <div>
+    <h1 className={clsx('text-xl', 'font-bold', 'text-blue-500', 'hover:text-blue-300', 'cursor-pointer')} >Hello from page.tsx</h1> 
+    </div> 
+  )
+} 
